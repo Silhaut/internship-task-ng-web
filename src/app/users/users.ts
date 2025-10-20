61525015e2ad75df08ee82cba2b27bb56838e3da
Silhaut <mnstirit@gmail.com>
2025-10-20 18:23:07 +0500

    feat(users): add filtering and pagination for users (#1)

--- src/app/users/users.ts
+++ src/app/users/users.ts
@@ -1,41 +1,91 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, debounceTime, switchMap } from 'rxjs';
 import { UserDto } from '../_dto/user.dto';
 import { UsersService } from '../_services/users';
-import { PagedDataDto } from '../_dto/paged-data.dto';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzFlexModule } from 'ng-zorro-antd/flex';
 
 @Component({
   selector: 'app-users',
   imports: [
     CommonModule,
+    NzPageHeaderModule,
+    NzFlexModule,
+    NzDividerModule,
+    NzTableModule,
+    NzCollapseModule,
+    NzSelectModule,
+    NzInputModule,
+    NzFormModule,
+    NzButtonModule,
+    ReactiveFormsModule,
+    NzTagModule,
   ],
   templateUrl: './users.html',
   styleUrl: './users.scss'
 })
 export class Users implements OnInit {
-  private usersService = inject(UsersService)
+  private usersService = inject(UsersService);
+  private fb = inject(FormBuilder);
 
-  page: number = 1
-  size: number = 20;
-  totalPage!: number;
-  totalSize!: number;
-  users$!: Observable<UserDto[]>;
+  users: UserDto[] = [];
+  total = 0;
+  loading = false;
+
+  query$ = new BehaviorSubject({
+    page: 1,
+    size: 20,
+    sort: 'createdAt',
+    order: 'desc' as 'asc' | 'desc',
+  });
+
+  filters = this.fb.group({
+    id: [''],
+    telegramId: [''],
+    username: [''],
+    firstName: [''],
+    lastName: [''],
+    role: [''],
+    phone: [''],
+  });
 
   ngOnInit() {
-    this.loadUsers();
+    this.filters.valueChanges
+      .pipe(debounceTime(400))
+      .subscribe((filters) => {
+        this.query$.next({
+          ...this.query$.value,
+          ...filters,
+          page: 1,
+        });
+      });
+
+    this.query$
+      .pipe(
+        switchMap((query) => {
+          this.loading = true;
+          return this.usersService.getUsers(query);
+        })
+      )
+      .subscribe((res) => {
+        console.debug('[Users] Query sent:', this.query$.value);
+        this.users = res.data;
+        this.total = res.totalSize;
+        this.loading = false;
+      });
   }
 
-  loadUsers() {
-    this.usersService.getUsers({
-      page: this.page,
-      size: this.size,
-      sort: 'createdAt',
-      order: 'desc',
-    }).subscribe((result: PagedDataDto<UserDto>) => {
-      this.users$ = of(result.data);
-      this.totalPage = result.totalPage;
-      this.totalSize = result.totalSize;
-    });
+  resetFilters() {
+    this.filters.reset();
   }
 }
