ec96d5d75d5300261ffe2e2207a5b8b6283d9ea4
Silhaut <mnstirit@gmail.com>
2025-10-21 17:08:09 +0500

    feat: add role guard (#9)

--- src/app/_services/auth.ts
+++ src/app/_services/auth.ts
@@ -1,5 +1,7 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { PagedDataDto } from '../_dto/paged-data.dto';
+import { UserDto } from '../_dto/user.dto';
 
 type LoginRequest = {
   username: string,
@@ -17,8 +19,29 @@ export class AuthService {
   private http = inject(HttpClient);
   private apiUrl = 'http://localhost:3000/api'
 
+  private meSignal = signal<UserDto | null | undefined>(undefined);
+
+  readonly me = this.meSignal.asReadonly();
+
+  async fetchCurrentUser() {
+    try {
+      const user = await this.http
+        .get<UserDto>(`${this.apiUrl}/auth/me`)
+        .toPromise();
+      this.meSignal.set(user);
+    } catch (err) {
+      console.error('Failed to fetch current user:', err);
+      this.meSignal.set(null);
+    }
+  }
+
   login(data: LoginRequest) {
     return this.http.post<LoginResponse>(`${this.apiUrl}/auth/login`, data)
+  }
+
+  logout() {
+    localStorage.removeItem('accessToken');
+    this.meSignal.set(null);
   }
 }
 
